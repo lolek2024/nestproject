e3798484d6dd601664d4fba0c6bc38fe136d501c
spicylolek <159866886+lolek2024@users.noreply.github.com>
2024-07-24 11:55:08 +0800

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UpdatedItemsModule } from './updated-items/updated-items.module';
+import { UpdatedItemsnewModule } from './updated-itemsnew/updated-itemsnew.module';
+
+@Module({
+  imports: [UpdatedItemsModule, UpdatedItemsnewModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
